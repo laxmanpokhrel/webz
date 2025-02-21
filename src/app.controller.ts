6bfae793040a2c6ec819d00cc11344344b75ca7f
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2025-02-21 13:04:17 +0545

    chore/docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,8 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // DEPENDENCY INJECTION
+  // Here the instance of AppService class is created injected in to this controller
   constructor(private readonly appService: AppService) {}
 
   @Get()
